79265e301357f2386a16b6e8d3292ace79bcaad5
Jose Andres Moya Porras <jose.91cr@gmail.com>
2021-11-08 08:01:26 -0600

    Changes to deployment

--- kafka_setup/entrypoint.sh
+++ kafka_setup/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo 'Waiting for Kafka to be ready...'
-cub kafka-ready -b kafka:9092 1 20
+until nc -vz 'kafka' 29092; do
+  >&2 echo "Waiting for Kafka to be ready... - sleeping"
+  sleep 2
+done
 echo 'Creating topics if they do not exist ...'
 kafka-topics --create --if-not-exists --zookeeper zookeeper:2181 --partitions 1 --replication-factor 1 --topic tasks
 kafka-topics --create --if-not-exists --zookeeper zookeeper:2181 --partitions 1 --replication-factor 1 --topic extractions
